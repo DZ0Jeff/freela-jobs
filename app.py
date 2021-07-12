7629de93a2a518240fbacd1ade29e63788e0bb30
Jeff <mx.jeferson.12@gmail.com>
2021-07-12 12:17:50 -0300

    add workana

--- app.py
+++ app.py
@@ -9,37 +9,61 @@ from time import sleep
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-def main():
+def send_99freela(telegram):
     """
-    Insert yout code here
+    Send 99freela jobs to telegram
+
+    :telegram: => telegram client
     """
-    telegram = TelegramBot(ROOT_DIR)
-
-    print('> iniciando robô...', end="\n")
-    soap = init_crawler('https://www.99freelas.com.br/projects?q=rob%C3%B4')
-    job_list = soap.find('ul', class_="result-list")
-    # print(job_list.prettify())
-
-    # jobs_to_send = []
+    freela_99 = init_crawler('https://www.99freelas.com.br/projects?q=rob%C3%B4')
+    job_list = freela_99.find('ul', class_="result-list")
+   
     print('> Pegando as informações...', end="\n")
     for job in job_list.find_all('li'):
-        # print(job.text)
+
         link = job.select_one('h1 a')['href']
         title = job.find('h1', class_="title").text
         client = job.find('p', class_="item-text client").text
         description = job.find('div', class_="item-text description formatted-text").text
-
-        # print('-'*60)
-        # print('Link: ',link)
-        # print('Título: ',title)
-        # print('Descrição: ',remove_whitespaces(description))
-        # print('Cliente: ', remove_whitespaces(client))
-        # print('\n') 
         telegram.send_message(f"\nTítulo: {remove_whitespaces(title)}\n\nCliente: {remove_whitespaces(client)}\n\nDescrição: {remove_whitespaces(description)}\n\nLink: {link}\n\n")
 
 
+def send_workana(telegram):
+    """
+    Send workana jobs to telegram
+
+    :telegram: => telegram client
+    """
+    base_link_workana = "https://www.workana.com"
+    workana = init_crawler('https://www.workana.com/jobs?category=it-programming&language=pt&query=Crawler')
+    projects = workana.find('div', id="projects")
+    
+    print('> Pegando as informações...', end="\n")
+    for project in projects.find_all('div', class_="project-item"):
+    
+        link = base_link_workana + project.find('a')['href']
+        title = project.find('h2').text
+        details = project.find('div', class_="html-desc project-details").get_text(separator=" ")
+
+        workana_msg = f"Título: {remove_whitespaces(title)}\n\nDetalhes: {details}\n\nLink: {link}"
+        telegram.send_message(workana_msg)
+
+
+def main():
+    """
+    send freela jobs to telegram
+    """
+    telegram = TelegramBot(ROOT_DIR)
+
+    print('> iniciando robô...', end="\n")
+    send_99freela(telegram)
+    send_workana(telegram)
+
+    
 if __name__ == "__main__":
-    schedule.every().day.at("09:00").do(main)
+    schedule.every().monday.at("12:00").do(main)
+    schedule.every().wednesday.at("12:00").do(main)
+    schedule.every().friday.at("12:00").do(main)
 
     while True:
         schedule.run_pending()
