e30c840c84014686741bf6fa3422f06dcc762267
Jeff <mx.jeferson.12@gmail.com>
2021-12-13 11:21:49 -0300

    add database to workana and get all projects

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # from utils.running import load_status, write_status
 from bs4 import BeautifulSoup
+from sqlalchemy.sql.expression import true
 from utils.telegram import TelegramBot
 from utils.parser_handler import init_crawler, remove_whitespaces
 import os
@@ -11,12 +12,10 @@ from src.database import DataStorage
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 job_storage = DataStorage()
-JOBS_99FRELLA = []
-WORKANA_JOBS = []
-FILTERS = ["bot", "robo", "scrapper", "crawler", "scrap", "scrapy", "beautifulsoap", "raspagem", "raspagem", "extração"]
+FILTERS = ["bot", "robo", "scrapper", "robô", "telegram", "crawler", "scrap", "scrapy", "beautifulsoap", "raspagem", "raspagem", "extração"]
 
 
-def send_99freela(telegram):
+def send_99freela(telegram, saved_links):
     """
     Send 99freela jobs to telegram
 
@@ -26,9 +25,6 @@ def send_99freela(telegram):
 
     base_link = 'https://www.99freelas.com.br'
     page = 1
-    
-    links = job_storage.select_by_link()
-    saved_links = [link[0] for link in links if link != None]
 
     while True:
         try:
@@ -51,7 +47,6 @@ def send_99freela(telegram):
                         print('Inserindo no banco de dados...')
                         job_storage.insert(title, link, client, description)
 
-                        JOBS_99FRELLA.append(link)
                         msg:str = f"\nTítulo: {remove_whitespaces(title)}\n\nCliente: {remove_whitespaces(client)}\n\nDescrição: {remove_whitespaces(description)}\n\nLink: {link}\n\n"
                         telegram.send_message(msg)
 
@@ -65,7 +60,7 @@ def send_99freela(telegram):
             break
 
 
-def send_workana(telegram):
+def send_workana(telegram, saved_links):
     """
     Send workana jobs to telegram
 
@@ -74,39 +69,58 @@ def send_workana(telegram):
     print('Iniciando workana...')
 
     base_link_workana = "https://www.workana.com"
-    workana = init_crawler('https://www.workana.com/jobs?category=it-programming&language=pt&query=Crawler')
-    
-    if not workana:
-        print('Erro do site saindo...')
-        return
-    
-    projects = workana.find('div', id="projects")
-    
-    print('> Pegando as informações...', end="\n")
-    for project in projects.find_all('div', class_="project-item"):
-    
-        link = base_link_workana + project.find('a')['href']
-        title = project.find('h2').text
-        details = project.find('div', class_="html-desc project-details").get_text(separator=" ")
+    page = 1
+    breakpoint_pagination = ""
 
-        if not link in WORKANA_JOBS and link != '':
-            WORKANA_JOBS.append(link)
-            workana_msg = f"Título: {remove_whitespaces(title)}\n\nDetalhes: {details}\n\nLink: {link}"
-            telegram.send_message(workana_msg)
+    while True:
+        # workana = init_crawler('https://www.workana.com/jobs?category=it-programming&language=pt&query=Crawler')
+        print(f'> página: {page}')
+        workana = init_crawler(f'https://www.workana.com/jobs?category=it-programming&language=pt&page={page}')
+        
+        if not workana:
+            print('Erro do site saindo...')
+            return
+        
+        projects = workana.find('div', id="projects")
+        
+        print('> Pegando as informações...', end="\n")
+        for project in projects.find_all('div', class_="project-item"):
 
-        else:
-            print('> Vaga já enviada!', end="\r")
+            link = base_link_workana + project.find('a')['href']
+            title = project.find('h2').text
+            if re.compile('|'.join(FILTERS),re.IGNORECASE).search(r"\b{}\b".format(title.split())):
+                details = project.find('div', class_="html-desc project-details").get_text(separator=" ")
+                
+                # if 404 fails use the breakpoint to stop pagination
+                if breakpoint_pagination == link:
+                    print('> Limite alcançado!')
+                    break
+                else:
+                    breakpoint_pagination = link
+
+                if not link in saved_links and link != '':
+                    job_storage.insert(title, link, '', details)
+                    workana_msg = f"Título: {remove_whitespaces(title)}\n\nDetalhes: {details}\n\nLink: {link}"
+                    telegram.send_message(workana_msg)
+
+            else:
+                print('> Vaga já enviada!', end="\r")
+        
+        page += 1
 
 
 def main():
     """
     send freela jobs to telegram
     """
-    telegram = TelegramBot(ROOT_DIR)
-
     print('> iniciando robô...', end="\n")
-    send_99freela(telegram)
-    send_workana(telegram)
+    telegram = TelegramBot(ROOT_DIR)
+    links = job_storage.select_by_link()
+    saved_links = [link[0] for link in links if link != None]
+
+    print('> extraíndo trabalhos...')
+    send_99freela(telegram, saved_links)
+    send_workana(telegram, saved_links)
     
     
 if __name__ == "__main__":
